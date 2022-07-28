13a2768e54a78ed27a8e597b7592e75cf48d63d6
Savindu Supun <savindusupunsathsara@gmail.com>
2022-07-28 20:56:55 +0530

    :sparkles: Added 20-2

---
+++ Course Work Past Papers/20-2.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int ID, smsUsage, callUsage, freeMins;
+    float smsCharge, callCharge, rental, totBill, total = 0;
+    char type;
+    for (int i = 1; i > 0; i++)
+    {
+        printf("Enter Customer %d ID: ", i);
+        scanf("%d", &ID);
+        if (ID == -111)
+        {
+            break;
+        }
+        printf("Enter Package type(A,B,C): ");
+        scanf("%s", &type);
+        if (type == 'A')
+        {
+            callCharge = 1.50;
+            smsCharge = 0.75;
+            rental = 150;
+            freeMins = 500;
+        }
+        else if (type == 'B')
+        {
+            callCharge = 1.75;
+            smsCharge = 0.50;
+            rental = 200;
+            freeMins = 800;
+        }
+        else if (type == 'C')
+        {
+            callCharge = 2.00;
+            smsCharge = 0.80;
+            rental = 300;
+            freeMins = 1000;
+        }
+        else
+        {
+            printf("Invalid Package Type!!!\n");
+            main();
+        }
+        printf("Enter Number of SMS: ");
+        scanf("%d", &smsUsage);
+        printf("Enter number of outgoing minutes: ");
+        scanf("%d", &callUsage);
+        callUsage = callUsage - freeMins;
+        if (callUsage < 0)
+        {
+            callUsage = 0;
+        }
+
+        totBill = rental + (callUsage * callCharge) + (smsUsage * smsCharge);
+        printf("User %d's monthly rental is %.2f\n", ID, totBill);
+        printf("----------------\n");
+        totBill += totBill;
+    }
+    printf("All customers Bill total is %.2f", totBill);
+}
